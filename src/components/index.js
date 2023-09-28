d1c239ec17eb1e89dfbcfbbfa3558ac17ae4c7d5
Md Musfikur Rahman <107528767+Md-Musfikur-Rahman@users.noreply.github.com>
2023-09-28 13:23:36 +0600

    init

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Main from "./Main";
+import Footer from "./Footer";
+
+export { Navbar, Hero, Main, Footer };
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      yellow: "#fbffa7",
+      orange: "#ff764d",
+      sky: "#b1c5ff",
+      blue: "#0000ff",
+      pink: "#d5b3ff",
+      green: "#b6ffc0",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
